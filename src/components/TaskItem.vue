55955bac19c1f1c10c036a975ef76b93c85ffe8f
Matt Del Signore <mattdelsig@me.com>
2019-12-09 21:43:02 -0500

    Changed the tasklist to filter out completed and non completed tasks

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,21 +1,49 @@
 <template>
-  <div class="task-item">{{ task.taskText }}</div>
+  <div class="task-item">
+    <input type="checkbox" id="task-checkbox" v-model="isCompleted" />
+    <p>{{ task.taskText }}</p>
+  </div>
 </template>
 
 <script lang="ts">
-import { createComponent } from '@vue/composition-api';
+import {
+  createComponent,
+  SetupContext,
+  ref,
+  watch,
+} from '@vue/composition-api';
 import { Task } from '../task';
 
 export default createComponent({
   props: { task: Object },
+  setup(props: { task: Task }, context) {
+    const { isCompleted } = useTaskCompletion(context, props.task.done);
+    return { isCompleted };
+  },
 });
+
+/** Setup the task completion code */
+function useTaskCompletion(context: SetupContext, initialValue: boolean) {
+  const isCompleted = ref(initialValue);
+
+  watch(() => {
+    // Emit an event to the parent component when the complete value changes
+    context.emit('update:done', isCompleted.value);
+  });
+  return { isCompleted };
+}
 </script>
 
 <style scoped>
 .task-item {
   display: flex;
   flex-direction: row;
+  align-items: center;
   padding: 0.5em;
   border-bottom: 1px solid #cccccc;
+}
+
+.task-item input {
+  margin-left: 1em;
 }
 </style>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -6,8 +6,23 @@
       <button type="button" v-on:click="addTask()">Add task</button>
     </div>
     <div class="list">
-      <h2>Task List</h2>
-      <task v-for="task in taskList" :key="task.id" :task="task" />
+      <h2>To Do</h2>
+      <task
+        v-for="task in nonCompletedTasks"
+        :key="task.id"
+        :task="task"
+        v-bind:done.sync="task.done"
+      />
+    </div>
+
+    <div class="list">
+      <h2>Done</h2>
+      <task
+        v-for="task in completedTasks"
+        :key="task.id"
+        :task="task"
+        v-bind:done.sync="task.done"
+      />
     </div>
   </main>
 </template>
@@ -19,34 +34,53 @@ import {
   Ref,
   reactive,
   watch,
+  computed,
 } from '@vue/composition-api';
 import TaskItem from 'Task.vue';
 import { Task } from '../task';
 
 export default createComponent({
   setup() {
-    const { taskList } = useTaskList();
+    const { taskList, nonCompletedTasks, completedTasks } = useTaskList();
     const { newTaskText, addTask } = useAddTask(taskList);
-    return { taskList, newTaskText, addTask };
+
+    return {
+      taskList,
+      newTaskText,
+      addTask,
+      nonCompletedTasks,
+      completedTasks,
+    };
   },
 });
 
 /** Add the code needed to handle rendering a list of tasks */
 function useTaskList() {
   // List of task's we're going to render. Use reactive() to make the reactivity deep.
-  const taskList = reactive<Task[]>([]);
-  return { taskList };
+  const taskList = ref<Task[]>([]);
+
+  // Filter out the two lists based on the completed status
+  const nonCompletedTasks = computed(() =>
+    taskList.value.filter((task) => task.done === false)
+  );
+  const completedTasks = computed(() =>
+    taskList.value.filter((task) => task.done === true)
+  );
+
+  return { taskList, nonCompletedTasks, completedTasks };
 }
 
 /** Adds the code needed to add tasks to the task list */
-function useAddTask(taskList: Task[]) {
+function useAddTask(taskList: Ref<Task[]>) {
   const newTaskText = ref(''); // Text for the input element
 
   function addTask() {
     // Generate a new id lazily
     const newId = Math.round(Math.random() * 100);
-    // Add the new task
-    taskList.push({ taskText: newTaskText.value, id: newId, done: false });
+    // Add the new task and make sure to keep reactivity!
+    taskList.value.push(
+      reactive({ taskText: newTaskText.value, id: newId, done: false })
+    );
     // Clear the input
     newTaskText.value = '';
   }
